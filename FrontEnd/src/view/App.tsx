1ab07c78f077bc90d98e0dec1eebd8123dfef953
chiii <chiii@chiiideMacBook-Air.local>
2025-01-03 00:52:44 +0800

    FrontEnd

--- FrontEnd/src/view/App.tsx
+++ FrontEnd/src/view/App.tsx
@@ -1,52 +1,12 @@
-import { useEffect, useRef, useState } from 'react'
-import '../style/App.css'
-import { asyncGet } from '../utils/fetch'
-import { api } from '../enum/api'
-import { Student } from '../interface/Student'
-import { resp } from '../interface/resp'
+import React from 'react';
+import MarathonBanner from './MarathonBanner';
 
-function App() {
-
-  const [students, setStudents] = useState<Array<Student>>([])
-
-  const cache = useRef<boolean>(false)
-
-  useEffect(() => {
-    /**
-     * 做緩存處理, 避免多次發起請求
-     */
-    if (!cache.current) {
-      cache.current = true;
-      asyncGet(api.getAll).then((res: resp<Array<Student>>) => {
-        if (res.code == 200) {
-          setStudents(res.body)
-        }
-      });
-    }
-  }, [])
-
-  const studentList = students ? students.map((student: Student) => {
-    return (
-      <div className='student' key={student._id}>
-        <p>帳號: {student.userName}</p>
-        <p>座號: {student.sid}</p>
-        <p>姓名: {student.name}</p>
-        <p>院系: {student.department}</p>
-        <p>年級: {student.grade}</p>
-        <p>班級: {student.class}</p>
-        <p>Email: {student.Email}</p>
-        <p>缺席次數: {student.absences ? student.absences : 0}</p>
-      </div>
-    )
-  }) : "loading"
-
+const App: React.FC = () => {
   return (
-    <>
-      <div className="container">
-        {studentList}
-      </div>
-    </>
-  )
-}
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <MarathonBanner />
+    </div>
+  );
+};
 
-export default App
+export default App;
