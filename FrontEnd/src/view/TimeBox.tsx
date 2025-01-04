b56e811e19fbc236eaeb887079cc4d6d108c3062
張安琪 <113300483+changcherry@users.noreply.github.com>
2025-01-05 00:23:20 +0800

    Create TimeBox.tsx

---
+++ FrontEnd/src/view/TimeBox.tsx
@@ -0,0 +1,16 @@
+//數字組件
+import React from 'react';
+import styles from '../style/TimeBox.modul.css';
+
+interface TimerBoxProps {
+  value: number; // 傳入的數值
+}
+
+export const TimerBox: React.FC<TimerBoxProps> = ({ value }) => (
+  <div className={styles.timerBox}>
+    {value.toString().padStart(2, '0')} {/* 確保數字是兩位，例如 01, 09 */}
+  </div>
+);
+
+
+export default TimerBox;
